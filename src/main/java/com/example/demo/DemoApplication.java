87ab30b4edbb035fd22740ce151f6d12554fe0ed
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-03-26 11:36:12 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-
+import java.sql.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,14 @@ public class DemoApplication {
         	String dbPassword = "password123"; // Hardcoded password (Security issue)
        		System.out.println("Connecting to DB with user: " + dbUser);
 		SpringApplication.run(DemoApplication.class, args);
+
+		String userInput = "admin' OR '1'='1"; // Dangerous input
+        	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+        	Statement stmt = conn.createStatement();
+        	ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + userInput + "'"); // SQL Injection
+        	while (rs.next()) {
+            System.out.println(rs.getString("username"));
+	        }	
 	}
 
 	@RequestMapping("/")
