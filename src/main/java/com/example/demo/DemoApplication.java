f7c754aefa424b7240f0cfe9356595f9a77a680d
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-01 12:22:20 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	public class CommandInjection {
+    public static void main(String[] args) throws IOException {
+        String userInput = "rm -rf /"; // Simulating an attacker’s input
+        Runtime.getRuntime().exec("sh -c " + userInput); // Command injection
+    }
+}
 
 
 	@RequestMapping("/")
