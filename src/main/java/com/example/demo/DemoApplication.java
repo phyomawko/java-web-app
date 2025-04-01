ade44ccfa741e384da4bf80c80df1470ce3dc754
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-01 12:12:59 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,18 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		
-		SpringApplication.run(DemoApplication.class, args);
-
-		 String userInput = "admin' OR '1'='1"; // Dangerous input
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + userInput + "'"); // SQL Injection
-        while (rs.next()) {
-            System.out.println(rs.getString("username"));
-        }
-	}
 
 	@RequestMapping("/")
   public String home() {
