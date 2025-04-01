2d9d122d6a85fa2cba460db131367e2bbbf1e9f3
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-01 11:22:59 +0630

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -10,4 +10,16 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	public class SQLInjection {
+    public static void main(String[] args) throws Exception {
+        String userInput = "admin' OR '1'='1"; // Dangerous input
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + userInput + "'"); // SQL Injection
+        while (rs.next()) {
+            System.out.println(rs.getString("username"));
+        }
+    }
+}
+
 }
