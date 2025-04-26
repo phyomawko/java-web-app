804dec62ccbbb66f692aea00c71810278184a45e
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-26 09:34:25 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	public class SQLInjection {
-    public static void main(String[] args) throws Exception {
-        String userInput = "admin' OR '1'='1"; // Dangerous input
-        Connection conn = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "login", "");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + userInput + "'"); // SQL Injection
-        while (rs.next()) {
-            System.out.println(rs.getString("username"));
-        }
-    }
+	public String authenticate(String username, String password) {
+  // ....
+  MyUserDetailsService s1 = new MyUserDetailsService();
+  MyUserPrincipal u1 = s1.loadUserByUsername(username);
+
+  if(u1 == null) {
+    throw new BadCredentialsException(username+" doesn't exist in our database"); // Sensitive
+  }
+  // ....
 }
 
 
