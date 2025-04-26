dff34323828063339cac33da0cd5a6e51c553299
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-26 10:38:52 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.logging.Logger;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 	
+private static final Logger logger = Logger.getLogger(LoggerExample.class.getName());
 
+    public void logUser(String username, String password) {
+        logger.info("Logging in user: " + username + " with password: " + password); // 🔥 Hotspot
+    }
 
 
 
