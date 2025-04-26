e723f2dace3d33aabafdf1839f7bdad8676339ee
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-26 09:50:12 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,24 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	@Bean(name = "multipartResolver")
-public CommonsMultipartResolver multipartResolver() {
-  CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-  multipartResolver.setMaxUploadSize(104857600); // Sensitive (100MB)
-  return multipartResolver;
+	public class AuthService {
+
+    public void login(String username, String password) {
+        if (!"admin".equals(username)) {
+            // ✅ This will be marked as a "Security Hotspot"
+            throw new RuntimeException("User '" + username + "' not found");
+        }
+
+        if (!"secret".equals(password)) {
+            throw new RuntimeException("Incorrect password");
+        }
+    }
 }
 
-@Bean(name = "multipartResolver")
-public CommonsMultipartResolver multipartResolver() {
-  CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(); // Sensitive, by default if maxUploadSize property is not defined, there is no limit and thus it's insecure
-  return multipartResolver;
-}
-
-@Bean
-public MultipartConfigElement multipartConfigElement() {
-  MultipartConfigFactory factory = new MultipartConfigFactory(); // Sensitive, no limit by default
-  return factory.createMultipartConfig();
-}
 
 
 	@RequestMapping("/")
