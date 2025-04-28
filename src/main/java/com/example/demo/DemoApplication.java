89f2bef4763cf4120a62cd53abd00bdca00c8cf3
phyomawko <52078345+phyomawko@users.noreply.github.com>
2025-04-28 21:58:09 +0630

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,15 +12,6 @@ import javax.servlet.http.Cookie;
 public class DemoApplication {
 	
 
-public static void main(String[] args) throws Exception {
-        String userInput = "admin' OR '1'='1"; // Dangerous input
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + userInput + "'"); // SQL Injection
-        while (rs.next()) {
-            System.out.println(rs.getString("username"));
-        }
-    }
 
 public void generateSessionId() {
 Random random = new Random(); // Security Hotspot: Not cryptographically secure
